319dd7be5646941f33f25aba1fae1a5786e73c4c
Deepak Singh <137672814+deepak2k03@users.noreply.github.com>
2025-06-27 07:22:52 +0530

    Create 09_Post_Order_Traversal_Using_1_Stack.cpp

---
+++ 09_Post_Order_Traversal_Using_1_Stack.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*>s;
+        vector<int>v;
+        if(root==NULL) return v;
+        TreeNode* curr=root;
+        while(curr!=NULL || !s.empty()){
+            if(curr!=NULL){
+                s.push(curr);
+                curr=curr->left;
+            }
+            else{
+                TreeNode* temp=s.top()->right;
+                if(temp==NULL){
+                    temp=s.top();
+                    s.pop();
+                    v.push_back(temp->val);
+                    while(!s.empty() && temp==s.top()->right){
+                        temp=s.top();
+                        s.pop();
+                        v.push_back(temp->val);
+                    }
+                }
+                else curr=temp;
+            }
+        }
+        return v;
+    }
+};
