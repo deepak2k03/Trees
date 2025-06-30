8ccb0b178c97273ce4feb6646752f7bcc794920e
Deepak Singh <137672814+deepak2k03@users.noreply.github.com>
2025-06-30 07:54:42 +0530

    Create 27_Check_for_Children_Sum_Property.cpp

---
+++ 27_Check_for_Children_Sum_Property.cpp
@@ -0,0 +1,33 @@
+/*Complete the function below
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){80
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    // Function to check whether all nodes of a tree have the value
+    // equal to the sum of their child nodes.
+    int isSumProperty(Node *root) {
+        // Add your code here
+        if (root == NULL || (root->left == NULL && root->right == NULL)) return 1;
+        int child=0;
+        if(root->left) child+=root->left->data;
+        if(root->right) child+=root->right->data;
+        if ((root->data == child) &&
+            isSumProperty(root->left) &&
+            isSumProperty(root->right)) {
+            return 1;
+        } 
+        else return 0;
+    }
+};
